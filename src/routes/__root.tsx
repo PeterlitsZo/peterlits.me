cd5fe3dfb8522649591258ef1213195ddcd31dc9
Peterlits Zo <peterlitszo@gmail.com>
2026-06-27 18:34:36 +0800

    fix: Update default meta title.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -25,7 +25,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'peterlits.me',
       },
     ],
     links: [
